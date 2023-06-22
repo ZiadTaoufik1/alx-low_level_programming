c0659db56b2895d1bfad9effde28b39b60a48b2b
Ziad <taoufikziad0@gmail.com>
2023-06-21 18:00:38 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @m: integer to be computed
+ *
+ * Return: absolute value of n
+*/
+int	_abs(int m)
+{
+	return ((m < 0) ? -m : m);
+}
