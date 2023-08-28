7ca8083a16781369e37a6af6dd6b64ebafde4083
Ziad <taoufikziad0@gmail.com>
2023-08-28 15:18:06 -0700

    c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,55 @@
 #include "main.h"
-
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+#include <stdio.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
+ * last_index - calculate index of last character in a string recursively
+ * @s: The input string
  *
- * Return: 1 if it is, 0 it's not
+ * Return: The index of the last character
  */
+
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int mod);
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
+
+	return (n);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome recursively
+ * @s: The input string
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+
 int is_palindrome(char *s)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	int end = last_index(s);
+
+	return (check(s, 0, end - 1, end % 2));
 }
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
+ * check - Helper function to check if a string is a palindrome recursively
+ * @s: The input string
+ * @start: The starting index
+ * @end: The ending index
+ * @mod: The modulus value
  *
- * Return: length of the string
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
 
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
+int check(char *s, int start, int end, int mod)
 {
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
-}}
+	else if (s[start] != s[end])
+
+		return (0);
+	return (check(s, start + 1, end - 1, mod));
+}
